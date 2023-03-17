f624a06d580796466e4af5824a22b1cf125c3344
halitov <halitov.r.k@gmail.com>
2023-03-17 16:34:44 +0500

    2023-03-17T16:35 	modified:   pom.xml 	modified:   src/main/java/halitov/netlogy/Main.java

--- src/main/java/halitov/netlogy/Main.java
+++ src/main/java/halitov/netlogy/Main.java
@@ -1,7 +1,37 @@
 package halitov.netlogy;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static final String url = "https://api.nasa.gov/planetary/apod?api_key=Ad5FiPZWPYc2xB330H839WyrrNP3f349TJbsBBq4";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Program NASA Image");
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
+
+        Scanner scanner = new Scanner(response.getEntity().getContent());
+
+        System.out.println(scanner.nextLine());
     }
+
+
 }
