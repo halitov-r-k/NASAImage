6d29ce4032d2f5cd00c39764d372e24c104ff0d8
halitov-r-k <halitov.r.k@gmail.com>
2023-03-19 18:04:08 +0500

    2023-03-19T18:00 The end

--- src/main/java/halitov/netlogy/Main.java
+++ src/main/java/halitov/netlogy/Main.java
@@ -1,14 +1,15 @@
 package halitov.netlogy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class Main {
     public static final String url = "https://api.nasa.gov/planetary/apod?api_key=Ad5FiPZWPYc2xB330H839WyrrNP3f349TJbsBBq4";
@@ -28,9 +29,22 @@ public class Main {
 
         CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
 
-        Scanner scanner = new Scanner(response.getEntity().getContent());
+        //Scanner scanner = new Scanner(response.getEntity().getContent());
 
-        System.out.println(scanner.nextLine());
+        //System.out.println(scanner.nextLine());
+        ResponseNASA responseNASA = mapper.readValue(response.getEntity().getContent(), ResponseNASA.class );
+        //System.out.println(responseNASA);
+        String[] imageUrl = responseNASA.getUrl().split("/");
+        String fileName = imageUrl[imageUrl.length-1];
+
+
+        CloseableHttpResponse image = httpClient.execute(new HttpGet(responseNASA.getUrl()));
+        HttpEntity entity = image.getEntity();
+        if (entity != null) {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            entity.writeTo(fileOutputStream);
+            fileOutputStream.close();
+        }
     }
 
 
