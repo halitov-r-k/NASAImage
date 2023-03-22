a40e70b3bcbf648892238345dc4a1dcb2e15f2e8
halitov <halitov.r.k@gmail.com>
2023-03-22 14:39:38 +0500

    2023-03-22T14:30

--- src/main/java/halitov/netlogy/Main.java
+++ src/main/java/halitov/netlogy/Main.java
@@ -1,51 +1,10 @@
 package halitov.netlogy;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
-    public static final String url = "https://api.nasa.gov/planetary/apod?api_key=Ad5FiPZWPYc2xB330H839WyrrNP3f349TJbsBBq4&date=2023-03-19";
-    public static ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws TelegramApiException {
 
-    public static void main(String[] args) throws IOException {
-
-        System.out.println("Program NASA Image");
-
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
-                        .build())
-                .build();
-
-        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
-
-        //Scanner scanner = new Scanner(response.getEntity().getContent());
-
-        //System.out.println(scanner.nextLine());
-        ResponseNASA responseNASA = mapper.readValue(response.getEntity().getContent(), ResponseNASA.class );
-        //System.out.println(responseNASA);
-        String[] imageUrl = responseNASA.getUrl().split("/");
-        String fileName = imageUrl[imageUrl.length-1];
-
-
-        CloseableHttpResponse image = httpClient.execute(new HttpGet(responseNASA.getUrl()));
-        HttpEntity entity = image.getEntity();
-        if (entity != null) {
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            entity.writeTo(fileOutputStream);
-            fileOutputStream.close();
-        }
+        TelegramBot telegramBot = new TelegramBot();
     }
-
-
 }
--- src/main/java/halitov/netlogy/TelegramBot.java
+++ src/main/java/halitov/netlogy/TelegramBot.java
@@ -1,26 +1,75 @@
 package halitov.netlogy;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
-    private static final String BOT_TOKEN ="5727044463:AAFLV__0-mQxAGrrUgBjwuMqjPkXHBJzLAU";
+    private static final String BOT_TOKEN = "5727044463:AAFLV__0-mQxAGrrUgBjwuMqjPkXHBJzLAU";
     private static final String BOT_USERNAME = "RuslanHalitov_bot";
 
-    private static final String URL = "https://api.nasa.gov/planetary/apod?api_key=Ad5FiPZWPYc2xB330H839WyrrNP3f349TJbsBBq4&date=2023-03-19";
+    private static final String URL = "https://api.nasa.gov/planetary/apod?api_key=Ad5FiPZWPYc2xB330H839WyrrNP3f349TJbsBBq4";
+
+    private static long chatId;
+
+    public TelegramBot() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(this);
+
+    }
+
     @Override
     public String getBotUsername() {
-        return null;
+        return BOT_USERNAME;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return BOT_TOKEN;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage()) {
+            chatId = update.getMessage().getChatId();
 
+            switch (update.getMessage().getText()) {
+                case "/start":
+                    sendMessage("Привет я бот, который присылает картинку от NASA");
+                    break;
+                case "/help":
+                    sendMessage("Чтобы получить картинку введи /give");
+                    break;
+                case "/give":
+                    try{
+                        sendMessage(Utils.getUrl(URL));
+                    } catch (IOException e) {
+                        throw new RuntimeException((e));
+                    }
+                    break;
+                default:
+                    sendMessage("Моя твоя не понимай");
+
+
+            }
+
+        }
+    }
+
+    private void sendMessage(String messageText) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(messageText);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
